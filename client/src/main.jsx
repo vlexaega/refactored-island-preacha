62f8145bb632dcce46482d34cf92e5918d5c3e2e
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-08-17 23:42:07 -0400

    main.jsx updated with App and Home

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx';
+import Home from './pages/Home.jsx';
+
+
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import ProductList from "../components/ProductList";
+import CategoryMenu from "../components/CategoryMenu";
+import Cart from "../components/Cart";
+
+const Home = () => {
+    return (
+        <div className="container">
+            <CategoryMenu />
+        </div>
+    );
+};
+
+export default Home;
