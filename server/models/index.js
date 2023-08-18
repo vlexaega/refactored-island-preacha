f5f0251005705d15fecbf7f2ee0d9a44cdc5de0d
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-08-17 21:10:41 -0400

    adds the Product model

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,6 @@
 const User = require('./User');
 const Category = require('./Category');
+const Order = require('./Order');
+const Product = required('./Product');
 
-module.exports = { User, Category };
+module.exports = { User, Category, Order, Product };
